ae25ecc2949bb6e2f5904d52cfc730fdcf192c3f
sergi <sergi_font@ceroca.cat>
2026-02-04 17:54:23 +0100

    segundo commit

--- main.py
+++ main.py
@@ -21,3 +21,24 @@ Archivo de configuración de dependencias:
     pip freeze > requirements.txt
 Realizar todo el rato que se añadan dependencias nuevas
 '''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Rutes: 127.0.0.1:5000/inici
+@app.route("/")
+@app.route("/home")
+def iniciarApp():
+    return render_template("home.html")
+
+@app.route("/info")
+def darInfo():
+    return "info"
+
+@app.route("/contacto")
+def getContactos():
+    return render_template("contactos.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
