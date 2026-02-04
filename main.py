b5f3fea9d23adf3049ed9e711bbef9924b895910
sergi <sergi_font@ceroca.cat>
2026-02-04 17:12:35 +0100

    primer commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# 
+'''
+Instalación de dependencias utilizando el entorno virtual venv.
+El entorno formará parte del proyecto.
+
+Creación del entorno virtual:
+    python -m venv venv
+
+Comando para elegir un interprete:
+    Ctrl + shift + p 
+
+Para utilizarlo/activarlo:
+    venv\Scripts\activate
+Para desactivar:
+    deactivate
+
+Añadir flask como dependencia con el entorno activo:
+    pip install flask
+
+Archivo de configuración de dependencias:
+    pip freeze > requirements.txt
+Realizar todo el rato que se añadan dependencias nuevas
+'''
