860a93dd9eff02380db17470e2ae117055774cbb
sergi <sergi_font@ceroca.cat>
2026-02-11 17:14:27 +0100

    variables iniciales creadas

--- main.py
+++ main.py
@@ -20,17 +20,44 @@ Añadir flask como dependencia con el entorno activo:
 Archivo de configuración de dependencias:
     pip freeze > requirements.txt
 Realizar todo el rato que se añadan dependencias nuevas
-'''
 
+
+Descargar dependencias del proyecto a partir de requirements.txt:
+Primero hay que crear el entorno virtual. Luego:
+    pip install -r requirements.txt
+'''
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+datos_posts = [
+    {
+        "autor": "Nombre1",
+        "titulo": "Título del post 1",
+        "contenido": "Contenido del post 1",
+        "data_post": "11 de febrero de 2026 - 16:49"
+    },
+    {
+        "autor": "Nombre2",
+        "titulo": "Título del post 2",
+        "contenido": "Contenido del post 2",
+        "data_post": "11 de febrero de 2026 - 16:50"
+    }
+]
 
 # Rutes: 127.0.0.1:5000/inici
 @app.route("/")
 @app.route("/home")
 def iniciarApp():
-    return render_template("home.html")
+    return render_template(
+        "home.html", 
+        titulo_pagina="Página inicial", 
+        primer_parrafo="Hola buenos días",
+        datos=datos_posts
+        )
 
 @app.route("/info")
 def darInfo():
@@ -40,5 +67,6 @@ def darInfo():
 def getContactos():
     return render_template("contactos.html")
 
+## __name__ obtiene el valor "__main__" por defecto 
 if __name__ == "__main__":
     app.run(debug=True)
